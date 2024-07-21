33a7f1ef659268072469b79b9d30435b581a3132
Lisa.Ly <lisa.y.ly1688@gmail.com>
2024-07-21 12:09:30 +0200

    in Progress

--- src/components/Curated/Curated.tsx
+++ src/components/Curated/Curated.tsx
@@ -10,13 +10,44 @@ const Curated = () => {
             <p>A selection of handmade jewels, Swiss made watches of haute horlogerie and unique objects.</p>
             <div className="curatedPics">
                 {/* klassen vergeben und die bilder je nach anforderung anpassen ! */}
-                <img className="goldUhr" src={goldUhr} alt="" /> 
-                <img className="diamantRing" src={diamantRing} alt="" />
-                <img className="raumDuft" src={raumDuft} alt="" />
-            </div>
-        </div>
+                <div className="container goldUhr">
+                    <img src={goldUhr} alt="Goldene Uhr"/>
+                    <div className="overlay">
+                        <div>Humbolt</div>
+                        <br />
+                        <div className ="line"></div>
+                        <br />
+                        <a href="http://google.com">Discover More</a>
+                    </div>
 
-        
+                </div>
+
+                <div className="container diamantRing">
+                    <img src={diamantRing} alt="Diamant Ring"/>
+                    <div className="overlay">
+                        <div>Rings</div>
+                        <br />
+                        <div className ="line"></div>
+                        <br />
+                        <a href="http://google.com">Discover More</a>
+                    </div>
+                </div>
+
+                <div className="container raumDuft">
+                    <img src={raumDuft} alt="Raumduft"/>
+                    <div className="overlay">
+                        <div>Locheber</div>
+                        <div>Milano</div>
+                        <br />
+                        <div className ="line"></div>
+                        <br />
+                        <a href="http://google.com">Discover More</a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
      );
 }
  
